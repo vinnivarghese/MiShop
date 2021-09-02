5a9b5bc2b6440dc1842a5b28fa69944e6e301b8d
vinni varghese <vinni.varghese@bidenergy.com>
2021-09-02 21:32:10 +0530

    Initial solution setup

---
+++ MiShop/MiShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiShop.WebUI.MvcApplication" Language="C#" %>
---
+++ MiShop/MiShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiShop.WebUI.Startup))]
+namespace MiShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
